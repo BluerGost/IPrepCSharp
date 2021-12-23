a15c93397e5cd8f953a833f09098a0ed74666af9
Kevin Stephen Biswas <kevinstephenbiswas@gmial.com>
2021-12-23 16:46:05 +0900

    Added first implementation of stack

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -4,3 +4,15 @@ using Stack;
 
 
 IStackCustom<int> stack = new StackCustom<int>();
+
+stack.Push(1);
+stack.Push(4);
+stack.Push(9);
+
+System.Console.WriteLine(stack.Peek());
+
+System.Console.WriteLine("Pop: " + stack.Pop());
+System.Console.WriteLine("Pop: " + stack.Pop());
+System.Console.WriteLine("Pop: " + stack.Pop());
+System.Console.WriteLine("Pop: " + stack.Pop()); // Exception
+
--- Stack/StackCustom.cs
+++ Stack/StackCustom.cs
@@ -8,24 +8,69 @@ namespace Stack
 {
     internal class StackCustom<T> : IStackCustom<T>
     {
-        public int MyProperty { get; set; }
+        /// <summary>
+        /// A single node in stack.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        private class Node<T>
+        {
+            internal T value;
+            internal Node<T> next;
+
+            public Node(T value, Node<T> next = null)
+            {
+                this.value = value;
+                this.next = next;
+            }
+        }
+
+        /// <summary>
+        /// Total number of items in stack.
+        /// </summary>
+        public int Count { get; set; }
+        
+        /// <summary>
+        /// Top of the stack
+        /// </summary>
+        private Node<T> _top;
+        
         public StackCustom()
         {
-
+            Count = 0;
         }
         public T Peek()
         {
-            throw new NotImplementedException();
+            return _top.value;
         }
 
         public T Pop()
         {
-            throw new NotImplementedException();
+            if (Count == 0)
+                throw new InvalidOperationException();
+
+            var result = _top.value;
+            _top = _top.next;
+            Count--;
+            return result;
         }
 
+        /// <summary>
+        /// Push the value at the top of the stack.
+        /// </summary>
+        /// <param name="value"></param>
         public void Push(T value)
         {
-            throw new NotImplementedException();
+            if(Count == 0)
+            {
+                _top = new(value);
+                Count++;
+                return;
+            }
+
+            var newNode = new Node<T>(value);
+            newNode.next = _top;
+            _top = newNode;
+            Count++;
         }
     }
 }
